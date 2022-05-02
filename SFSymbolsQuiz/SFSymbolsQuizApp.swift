489999d6b551d0f4e17eecfe9f86f7c986ef4d19
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2022-05-02 22:38:24 +0900

    Initial Commit

---
+++ SFSymbolsQuiz/SFSymbolsQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsQuizApp.swift
+//  SFSymbolsQuiz
+//
+//  Created by MacBook Pro M1 on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
